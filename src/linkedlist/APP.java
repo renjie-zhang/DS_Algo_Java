4790420d12a14c2f55b3d2e77d1084a60cb1985d
renjie.zhang@tianheng-uestc.com <renjie.zhang@tianheng-uestc.com>
2019-08-15 18:24:06 +0800

    add linkedlist

---
+++ src/linkedlist/APP.java
@@ -0,0 +1,48 @@
+package linkedlist;
+
+
+/**
+ * @ClassName APP
+ * @Description
+ * @Author joker
+ * @Date 2019/8/15 16:52
+ */
+public class APP {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.head = new LinkedList.Node(1);
+        LinkedList.Node NodeTwo = new LinkedList.Node(2);
+        LinkedList.Node NodeThree = new LinkedList.Node(3);
+         /* Three nodes have been allocated dynamically.
+          We have references to these three blocks as first ,
+          second and third
+
+          llist.head        second              third
+             |                |                  |
+             |                |                  |
+         +----+------+     +----+------+     +----+------+
+         | 1  | null |     | 2  | null |     |  3 | null |
+         +----+------+     +----+------+     +----+------+ */
+        list.head.next = NodeTwo;
+        /*  Now next of the first Node refers to the second.  So they
+            both are linked.
+
+         llist.head        second              third
+            |                |                  |
+            |                |                  |
+        +----+------+     +----+------+     +----+------+
+        | 1  |  o-------->| 2  | null |     |  3 | null |
+        +----+------+     +----+------+     +----+------+ */
+        NodeTwo.next = NodeThree;
+        /*  Now next of the second Node refers to third.  So all three
+            nodes are linked.
+
+         llist.head        second              third
+            |                |                  |
+            |                |                  |
+        +----+------+     +----+------+     +----+------+
+        | 1  |  o-------->| 2  |  o-------->|  3 | null |
+        +----+------+     +----+------+     +----+------+ */
+        list.printList();
+    }
+}
---
+++ src/linkedlist/LinkedList.java
@@ -0,0 +1,29 @@
+package linkedlist;
+
+/**
+ * @ClassName LinkedList
+ * @Description 链表
+ * @Author joker
+ * @Date 2019/8/15 16:51
+ */
+class LinkedList {
+    Node head;
+
+    static class Node{
+        int data;
+        Node next;
+        //构造方法
+        Node(int data){
+            this.data = data;
+            next = null;
+        }
+    }
+    void printList()
+    {
+        Node n = head;
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
+}
