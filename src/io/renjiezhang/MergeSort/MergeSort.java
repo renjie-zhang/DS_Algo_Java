2146be77cc69e8ae5fe9a2383212fcaec71ae875
renjie.zhang <joker_zhang@aliyun.com>
2019-09-17 22:40:42 +0800

    add mergesort

---
+++ src/io/renjiezhang/MergeSort/MergeSort.java
@@ -0,0 +1,76 @@
+package io.renjiezhang.MergeSort;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author: renjie.zhang
+ * @date:2019/9/17-21:35
+ * @Description: 归并排序
+ */
+public class MergeSort {
+    private static Comparable[] aux;
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[80000];
+        for (int i = 0; i < 80000; i++) {
+            arr[i] = (int) (Math.random() * 8000000);
+        }
+
+        System.out.println("");
+        Date data1 = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date1Str = simpleDateFormat.format(data1);
+        System.out.println("排序前" + date1Str);
+        aux = new Comparable[arr.length];
+        sort(arr,0,arr.length-1);
+
+        Date data2 = new Date();
+        String date2Str = simpleDateFormat.format(data2);
+        System.out.println("排序之后" + date2Str);
+    }
+    private static void sort(Comparable[] a,int low,int high){
+        if(high<=low) return;
+        int mid = low+(high-low)/2;
+        sort(a,low,mid);
+        sort(a,mid+1,high);
+        merge(a,low,mid,high);
+    }
+    private static void merge(Comparable[] a,int low,int mid,int high){
+        int i=low,j=mid+1;
+        for(int k=low;k<=high;k++)
+            aux[k]=a[k];
+        for(int k=low;k<=high;k++)
+            if(i>mid) a[k]=aux[j++];
+            else if (j>high) a[k]=aux[i++];
+            else if (less(aux[j],aux[i])) a[k]=aux[j++];
+            else a[k]=aux[i++];
+    }
+
+    private static boolean less(Comparable v,Comparable w){
+        return v.compareTo(w)<0;
+    }
+    private static void exchange(Object[] a,int i,int j){
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+    private static boolean isSorted(Comparable[] a){
+        for(int i= 0 ;i<a.length;i++){
+            if(less(a[i],a[i-1])) return false;
+        }
+        return true;
+    }
+
+    private static boolean isHsorted(Comparable[] a,int h){
+        for(int i = 0;i<a.length;i++){
+            if(less(a[i],a[h])) return false;
+        }
+        return true;
+    }
+
+    private static void show(Comparable[] a){
+        for(int i =0 ;i<a.length;i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
